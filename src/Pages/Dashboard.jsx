02ca337ba19f2bbcb07ec555a0c301b2020e36de
saipepu <saipepu.mdy257@gmail.com>
2023-03-14 22:01:36 +0700

    google sheet api

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import GSheet from '../Gsheet/GSheet'
 
 const Dashboard = () => {
   const navigation = useNavigate();
+  const data = GSheet;
+  console.log(data);
   return (
     <div>Dashboard
       <button onClick={() => navigation('/training')}>go to training</button>
