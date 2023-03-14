38230108be48e0906d8e78f5f439b92948944f95
saipepu <saipepu.mdy257@gmail.com>
2023-03-14 20:55:03 +0700

    gh-pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from './Pages/Dashboard';
+import Training from './Pages/Training';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" exact element={<Dashboard />} />
+        <Route path="/training" element={<Training />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
