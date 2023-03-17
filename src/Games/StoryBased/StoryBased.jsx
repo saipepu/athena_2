9876cf30699785b5148b20290061bbd906915264
Jan Poonthong <janpoonthong628@gmail.com>
2023-03-17 22:28:08 +0700

    WIP

--- src/Games/StoryBased/StoryBased.jsx
+++ src/Games/StoryBased/StoryBased.jsx
@@ -26,47 +26,31 @@ const StoryBased = () => {
   const [blockNextScene, setBlockNextScene] = useState(false);
 
   const [scenes, setScenes] = useState([]);
-
+  const navigation = useNavigate();
   console.log("Before useEffect...")
+  
 
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        console.log("Running useEffect...")
-        const data = await getSheetData();
-        // setScenes(data);
-        console.log(data);
-      } catch (error) {
-        console.log(error);
-      }
+      return await getSheetData();
     };
-    fetchData().then((data) => setScenes(data));
+    fetchData().then((data) => {
+      setScenes(data);
+      // setScenes(scenes => [...scenes, data]);
+      console.log("finish fetch")
+      console.table(scenes)});
     // fetchUsers().then((users) => setUsers(users));
   }, []);
 
-
-  // useEffect(() => {
-  //   console.log("Running useEffect...")
-  //   const fetchData = async () => {
-  //     try {
-  //       const data = await getSheetData();
-  //       setScenes(data);
-  //       console.log(data);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
-
-
-  console.log("After useEffect...")
-
-  const navigation = useNavigate();
+ 
 
   // Increase Progress bar
   useEffect(() => {
-    // console.log(nextSceneCount, scenes.length)
+    if (typeof scenes === 'undefined') return
+    if (scenes.length === 0) return
+    const bar = document.getElementById("bar");
+    bar.style.width = progressCount * 12.5 + "%";
+    
     if (nextSceneCount === scenes.length - 1) {
       setTimeout(() => {
         setGameOver((gameOver) => (gameOver = true));
@@ -77,13 +61,14 @@ const StoryBased = () => {
         }
       }, 1000);
     }
-    // console.log(score);
-  }, [nextSceneCount]);
-
-  // Increase Progress bar
-  useEffect(() => {
-    const bar = document.getElementById("bar");
-    bar.style.width = progressCount * 12.5 + "%";
+    if (scenes[nextSceneCount].scene === "question") {
+      console.log("Question scene");
+      if (click === true) {
+        setBlockNextScene((blockNextScene) => (blockNextScene = false));
+      } else {
+        setBlockNextScene((blockNextScene) => (blockNextScene = true));
+      }
+    }
   }, [nextSceneCount]);
 
   const nextScene = () => {
@@ -114,17 +99,6 @@ const StoryBased = () => {
     }
   };
 
-  useEffect(() => {
-    if (scenes[nextSceneCount].scene === "question") {
-      console.log("Question scene");
-      if (click === true) {
-        setBlockNextScene((blockNextScene) => (blockNextScene = false));
-      } else {
-        setBlockNextScene((blockNextScene) => (blockNextScene = true));
-      }
-    }
-  }, [nextSceneCount]);
-
   const submitAnswer = (goToQuestionSceneID, reward) => {
     setClick((click) => (click = true));
     setTimeout(() => {
@@ -137,233 +111,245 @@ const StoryBased = () => {
       }
     }, 1000);
   };
+  // if (!(typeof scenes === 'undefined')) {
+  //   if (scenes.length == 0) {
+  //     console.log("HERE111")
+  //     return (<div>loading</div>)
+  //   }
+  // }
 
-  if (scenes.length === 0) {
-    console.log("HERE")
-    return (<div>loading</div>)
+  function ScenesCheck() {
+    if (typeof scenes === 'undefined') return
+    if (scenes.length === 0) return
+    return true
   }
-
-  let isLast = nextSceneCount == scenes.length - 1;
+  let isLast = nextSceneCount == (scenes?.length - 1) ?? 0;
   // console.log(gameOver, blockNextScene);
-
   return (
-    <div className="wrapper">
+      <div className="wrapper">
+      {(ScenesCheck()) ? (
       <div
-        style={{
-          backgroundImage: `url(${scenes[nextSceneCount]?.background})`,
-        }}
-        className="Scontainer"
-      >
-        {/* Button  */}
-        {start ? (
-          <button
-            disabled={gameOver || blockNextScene}
-            className="start_btn"
-            onClick={nextScene}
-          >
-            {isLast ? "Done!" : start ? "Start!" : "Next!"}
-          </button>
-        ) : (
-          <button className="next_btn" onClick={nextScene}></button>
-        )}
+      style={{
+        backgroundImage: `url(${require(`../../assets/talky_talky_startscreen.png`)})`,
+      }}
+      className="Scontainer"
+    >
+      {/* Button  */}
+      {start ? (
+        <button
+          disabled={gameOver || blockNextScene}
+          className="start_btn"
+          onClick={nextScene}
+        >
+          {isLast ? "Done!" : start ? "Start!" : "Next!"}
+        </button>
+      ) : (
+        <button className="next_btn" onClick={nextScene}></button>
+      )}
 
-        {isLast || (!start && scenes[nextSceneCount]?.scene !== "start") ? (
-          <button
-            disabled={gameOver || blockNextScene}
-            className="back_btn"
-            onClick={() => backScene(scenes[nextSceneCount])}
-          ></button>
-        ) : null}
-        {start ? (
-          <h1 className="storyBasedtitle">{scenes[nextSceneCount].text}</h1>
-        ) : null}
+      {isLast || (!start && scenes[nextSceneCount]?.scene !== "start") ? (
+        <button
+          disabled={gameOver || blockNextScene}
+          className="back_btn"
+          onClick={() => backScene(scenes[nextSceneCount])}
+        ></button>
+      ) : null}
+      {start ? (
+        <h1 className="storyBasedtitle">{scenes[nextSceneCount].text}</h1>
+      ) : null}
 
-        {/* Process bar */}
-        <div className="progress_wrapper">
-          <div className="info">
-            <div className="name_container">
-              <p className="rank">A</p>
-              <p className="name">5 ATHENA</p>
-            </div>
-            <div className="avatar">
-              <img src={avatar} alt="avatar" className="avatar_img" />
-            </div>
+      {/* Process bar */}
+      <div className="progress_wrapper">
+        <div className="info">
+          <div className="name_container">
+            <p className="rank">A</p>
+            <p className="name">5 ATHENA</p>
           </div>
-          <div className="progress_container">
-            <div className="title">Experience Bar</div>
-            <div className="progress">
-              <img src={exp_img} alt="exp" className="exp_img" />
-              <div className="progress_bar">
-                <div className="bar" id="bar"></div>
-                <div className="xp">XP</div>
-              </div>
+          <div className="avatar">
+            <img src={avatar} alt="avatar" className="avatar_img" />
+          </div>
+        </div>
+        <div className="progress_container">
+          <div className="title">Experience Bar</div>
+          <div className="progress">
+            <img src={exp_img} alt="exp" className="exp_img" />
+            <div className="progress_bar">
+              <div className="bar" id="bar"></div>
+              <div className="xp">XP</div>
             </div>
           </div>
         </div>
+      </div>
 
-        {gameOver ? (
-          <div
-            style={{ zIndex: 20, backgroundImage: `url()` }}
-            className="model_wrapper"
-          >
-            <div className="gameOver">
-              <div className="media_container">
-                <img
-                  src={treasure_chest}
-                  alt="treasure_chest"
-                  className="media"
-                />
-              </div>
-              <div className="content">
-                <p className="title">
-                  You have earn <span>1 Token</span> and <span>{score} XP</span>{" "}
-                  today.
-                </p>
-              </div>
-              <div className="btn_gp">
-                <button
-                  className="go_to_home"
-                  onClick={() => {
-                    setGameOver(!gameOver);
-                    navigation("/dashboard");
-                  }}
-                >
-                  Go to Home
-                </button>
-                <button
-                  className="claim_reward"
-                  onClick={() => {
-                    setGameOver(!gameOver);
-                    navigation("/dashboard");
-                  }}
-                >
-                  Claim Reward
-                </button>
-              </div>
+      {gameOver ? (
+        <div
+          style={{ zIndex: 20, backgroundImage: `url()` }}
+          className="model_wrapper"
+        >
+          <div className="gameOver">
+            <div className="media_container">
+              <img
+                src={treasure_chest}
+                alt="treasure_chest"
+                className="media"
+              />
+            </div>
+            <div className="content">
+              <p className="title">
+                You have earn <span>1 Token</span> and <span>{score} XP</span>{" "}
+                today.
+              </p>
+            </div>
+            <div className="btn_gp">
+              <button
+                className="go_to_home"
+                onClick={() => {
+                  setGameOver(!gameOver);
+                  navigation("/dashboard");
+                }}
+              >
+                Go to Home
+              </button>
+              <button
+                className="claim_reward"
+                onClick={() => {
+                  setGameOver(!gameOver);
+                  navigation("/dashboard");
+                }}
+              >
+                Claim Reward
+              </button>
             </div>
           </div>
-        ) : (
-          <></>
-        )}
+        </div>
+      ) : (
+        <></>
+      )}
 
-        {start ? (
-          <div
-            style={{
-              backgroundImage: `url(${scenes[nextSceneCount].avatar})`,
-              backgroundSize: `${scenes[nextSceneCount].backgroundSize}px`,
-            }}
-            className="StartPosition"
-          ></div>
-        ) : (
-          <>
-            {scenes[nextSceneCount]?.scene === "employee" ? (
-              <>
-                <div className="employeeSpeakbubble">
-                  <p className="hisir">{scenes[nextSceneCount].text}</p>
-                </div>
-                <div
-                  style={{
-                    backgroundImage: `url(${scenes[nextSceneCount].avatar})`,
-                    backgroundSize: `${scenes[nextSceneCount].backgroundSize}px`,
-                  }}
-                  className="employeePosition"
-                ></div>
-              </>
-            ) : (
-              <div>
-                {scenes[nextSceneCount]?.scene === "question" ? (
-                  <>
-                    {scenes[nextSceneCount].avatar === undefined ? null : (
+      {start ? (
+        <>
+        {/* <img src={process.env.PUBLIC_URL + "/static/media/assets/talky_talky_startscreen.png"}/> */}
+        
+        {/* <img src={require(`../../assets/talky_talky_startscreen.png`)}/> */}
+        <div
+          style={{
+            backgroundImage: `url()`,
+            backgroundSize: `${scenes[nextSceneCount].backgroundSize}px`,
+          }}
+          className="StartPosition"
+        ></div>
+        </>
+      ) : (
+        <>
+          {scenes[nextSceneCount]?.scene === "employee" ? (
+            <>
+              <div className="employeeSpeakbubble">
+                <p className="hisir">{scenes[nextSceneCount].text}</p>
+              </div>
+              <div
+                style={{
+                  backgroundImage: `url(${scenes[nextSceneCount].avatar})`,
+                  backgroundSize: `${scenes[nextSceneCount].backgroundSize}px`,
+                }}
+                className="employeePosition"
+              ></div>
+            </>
+          ) : (
+            <div>
+              {scenes[nextSceneCount]?.scene === "question" ? (
+                <>
+                  {scenes[nextSceneCount].avatar === undefined ? null : (
+                    <div
+                      style={{
+                        backgroundImage: `url(${scenes[nextSceneCount].avatar})`,
+                        backgroundSize: `${scenes[nextSceneCount].backgroundSize}px`,
+                      }}
+                      className="employeePosition"
+                    ></div>
+                  )}
+                  <div className="containerquestion">
+                    <div className="questionScene">
+                      <p className="questionText">
+                        {scenes[nextSceneCount].question}
+                      </p>
+                    </div>
+                    <form id="submitform">
+                      {scenes.option.map((element, index) => (
+                        <div key={index} className="input_container">
+                          <input type="radio" id={index} name="radio" />
+                          <label
+                            onClick={() =>
+                              submitAnswer(
+                                scenes[nextSceneCount]
+                                  .goToQuestionSceneIDOption1,
+                                scenes[nextSceneCount].pointOption1
+                              )
+                            }
+                            className="correctanswer"
+                            htmlFor={index}
+                          >
+                            {index + 1}. {element.option}
+                          </label>
+                        </div>
+                      ))}
+                    </form>
+                  </div>
+                </>
+              ) : (
+                <div>
+                  {scenes[nextSceneCount]?.scene === "start" ? (
+                    <>
                       <div
                         style={{
                           backgroundImage: `url(${scenes[nextSceneCount].avatar})`,
                           backgroundSize: `${scenes[nextSceneCount].backgroundSize}px`,
                         }}
-                        className="employeePosition"
+                        className="StartPosition"
                       ></div>
-                    )}
-                    <div className="containerquestion">
-                      <div className="questionScene">
-                        <p className="questionText">
-                          {scenes[nextSceneCount].question}
-                        </p>
-                      </div>
-                      <form id="submitform">
-                        {scenes.option.map((element, index) => (
-                          <div key={index} className="input_container">
-                            <input type="radio" id={index} name="radio" />
-                            <label
-                              onClick={() =>
-                                submitAnswer(
-                                  scenes[nextSceneCount]
-                                    .goToQuestionSceneIDOption1,
-                                  scenes[nextSceneCount].pointOption1
-                                )
-                              }
-                              className="correctanswer"
-                              htmlFor={index}
-                            >
-                              {index + 1}. {element.option}
-                            </label>
+                      <h1 className="storyBasedtitle">
+                        {scenes[nextSceneCount].text}
+                      </h1>
+                      <button className="start_btn" onClick={nextScene}>
+                        Start!
+                      </button>
+                    </>
+                  ) : (
+                    <>
+                      {scenes[nextSceneCount]?.scene === "time" ? (
+                        <div className="timebox">
+                          <div className="questionScene">
+                            <p className="timetext">
+                              {scenes[nextSceneCount].text}
+                            </p>
                           </div>
-                        ))}
-                      </form>
-                    </div>
-                  </>
-                ) : (
-                  <div>
-                    {scenes[nextSceneCount]?.scene === "start" ? (
-                      <>
-                        <div
-                          style={{
-                            backgroundImage: `url(${scenes[nextSceneCount].avatar})`,
-                            backgroundSize: `${scenes[nextSceneCount].backgroundSize}px`,
-                          }}
-                          className="StartPosition"
-                        ></div>
-                        <h1 className="storyBasedtitle">
-                          {scenes[nextSceneCount].text}
-                        </h1>
-                        <button className="start_btn" onClick={nextScene}>
-                          Start!
-                        </button>
-                      </>
-                    ) : (
-                      <>
-                        {scenes[nextSceneCount]?.scene === "time" ? (
-                          <div className="timebox">
-                            <div className="questionScene">
-                              <p className="timetext">
-                                {scenes[nextSceneCount].text}
-                              </p>
-                            </div>
+                        </div>
+                      ) : (
+                        <>
+                          <div className="customerSpeakbubble">
+                            <p className="hisir">
+                              {scenes[nextSceneCount]?.text}
+                            </p>
                           </div>
-                        ) : (
-                          <>
-                            <div className="customerSpeakbubble">
-                              <p className="hisir">
-                                {scenes[nextSceneCount]?.text}
-                              </p>
-                            </div>
 
-                            <div
-                              style={{
-                                backgroundImage: `url(${scenes[nextSceneCount]?.avatar})`,
-                                backgroundSize: `${scenes[nextSceneCount]?.backgroundSize}px`,
-                              }}
-                              className="customerPosition"
-                            ></div>
-                          </>
-                        )}
-                      </>
-                    )}
-                  </div>
-                )}
-              </div>
-            )}
-          </>
-        )}
-      </div>
+                          <div
+                            style={{
+                              backgroundImage: `url(${scenes[nextSceneCount]?.avatar})`,
+                              backgroundSize: `${scenes[nextSceneCount]?.backgroundSize}px`,
+                            }}
+                            className="customerPosition"
+                          ></div>
+                        </>
+                      )}
+                    </>
+                  )}
+                </div>
+              )}
+            </div>
+          )}
+        </>
+      )}
+    </div>
+      ):(<div>loading</div>)}
     </div>
   );
 };
